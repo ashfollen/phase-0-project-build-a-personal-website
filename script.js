48c878f069a85d997e45c0f37133de327bf268ee
Ashley Follen <ashleyfollen@outlook.com>
2021-07-12 22:09:06 -0600

    JavaScript added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const calligraphy = document.getElementById('calligraphy');
 
+function clickAlert() {
+    alert('Have an upcoming event? Email me at ashleyfollen@outlook.com for custom, handwritten name cards and signage!');
+}
+
+calligraphy.addEventListener('click', clickAlert);
+
+const macrame = document.getElementById('macrame');
+
+function clickAlertTwo() {
+    alert("Looking for a custom piece? Email me at ashleyfollen@outlook.com and let's chat!");
+}
+
+macrame.addEventListener('click', clickAlertTwo);
